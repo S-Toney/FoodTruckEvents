abfc5dd8c45beab2d3830fbbf6d942af2753a743
S-Toney <stacy.toney@protonmail.com>
2021-01-26 12:37:59 -0600

    template additions

--- FTE.UI.MVC/App_Start/BundleConfig.cs
+++ FTE.UI.MVC/App_Start/BundleConfig.cs
@@ -20,11 +20,16 @@ namespace FTE.UI.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/venobox.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/site.css",
+                      "~/Content/style.css",
+                      "~/Content/custom.css"));
         }
     }
 }
