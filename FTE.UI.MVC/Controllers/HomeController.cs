aabc1c23341a433fabbd1e7b5a765e0e41c803ff
S-Toney <stacy.toney@protonmail.com>
2021-01-28 10:08:07 -0600

    Functionality added to contact form and successfully tested - email confirmation page also

--- FTE.UI.MVC/Controllers/HomeController.cs
+++ FTE.UI.MVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FTE.UI.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,15 +18,58 @@ namespace FTE.UI.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string emailBody = $"Email From: {cvm.Name}\nSubject: {cvm.Subject}\nRespond To: {cvm.Email}\nMessage: {cvm.Message}";
+
+            MailMessage msg = new MailMessage
+                (
+                //From
+                "no-reply@tessatoney.com",
+                //To (Where the actual message is sent)
+                "stacy.toney@protonmail.com",
+                //Subject
+                "Email from FTE's Contact Form",
+                //Body
+                emailBody
+                );
+
+            msg.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("mail.tessatoney.com");
+            client.Credentials = new NetworkCredential("no-reply@tessatoney.com", "20Cen$us20");//Change to ******** before pushing to GitHub but use password for deploy
+
+            try
+            {
+                client.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = $"Sorry, something went wrong. Error Message: {ex.Message}<br />{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+        }
+
+        [HttpGet]
+        public ActionResult EmailConfirmation()
+        {
             return View();
         }
     }
---
+++ FTE.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FTE.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "* Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
