62607852c3c9a2a0f9acbd6f55a9da3141c1dcae
Stacy Toney <stacy.toney@protonmail.com>
2022-06-29 10:09:59 -0500

    connection string adjusted for new server - data coming in for events but logins and resgisters fail

--- FTE.UI.MVC/Controllers/AccountController.cs
+++ FTE.UI.MVC/Controllers/AccountController.cs
@@ -70,8 +70,8 @@ namespace IdentitySample.Controllers
                 return View(model);
             }
 
-            // This doen't count login failures towards lockout only two factor authentication
-            // To enable password failures to trigger lockout, change to shouldLockout: true
+            /* This doen't count login failures towards lockout only two factor authentication
+             To enable password failures to trigger lockout, change to shouldLockout: true */
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
